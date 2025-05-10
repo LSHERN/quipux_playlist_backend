1e02353ea5e91f008f879eb01523d91fade92322
LSHERN <lshernandez@unimayor.edu.co>
2025-05-10 08:54:08 -0500

    "Add logic for authentication"

---
+++ src/main/java/config/services/UserService.java
@@ -0,0 +1,8 @@
+package config.services;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService {
+
+    UserDetailsService userDetailsService();
+}
